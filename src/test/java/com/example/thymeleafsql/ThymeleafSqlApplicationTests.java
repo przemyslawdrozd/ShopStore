c18ab35412618535e0159fe8797ca442e638b15f
przemo06133 <przemo06133@gmail.com>
2020-01-16 19:41:36 +0100

    Initial commit

---
+++ src/test/java/com/example/thymeleafsql/ThymeleafSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
