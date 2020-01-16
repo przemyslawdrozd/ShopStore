c7c789e7bb785b9a2ec72f70b6063392a008add1
przemo06133 <przemo06133@gmail.com>
2020-01-16 23:19:29 +0100

    connection with h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO item VALUES(5, CURRENT_DATE, 'orange', 3.15, 3);
+INSERT INTO item VALUES(6, '2010-12-09', 'apple', 3.45, 5);
+INSERT INTO item VALUES(7, CURRENT_DATE, 'cherry', 3.15, 1);
+INSERT INTO item VALUES(8, CURRENT_DATE, 'nuts', 3.15, 1);
