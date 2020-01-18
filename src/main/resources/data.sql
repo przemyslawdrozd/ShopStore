ff5d4bf1e60831d3167276021a8666640ccb3758
przemo06133 <przemo06133@gmail.com>
2020-01-18 16:53:40 +0100

    Show generated query / insert new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,12 @@
-INSERT INTO item VALUES(5, CURRENT_DATE, 'orange', 3.15, 3);
-INSERT INTO item VALUES(6, '2010-12-09', 'apple', 3.45, 5);
-INSERT INTO item VALUES(7, CURRENT_DATE, 'cherry', 3.15, 1);
-INSERT INTO item VALUES(8, CURRENT_DATE, 'nuts', 3.15, 1);
+INSERT INTO item VALUES(1, CURRENT_DATE, 'orange', 3.15, 3);
+INSERT INTO item VALUES(2, '2010-12-09', 'apple', 3.45, 5);
+INSERT INTO item VALUES(3, CURRENT_DATE, 'cherry', 3.15, 1);
+INSERT INTO item VALUES(4, CURRENT_DATE, 'nuts', 3.15, 1);
 
 INSERT INTO employee VALUES(1, 'admin', '123');
+
+INSERT INTO cust_order VALUES(1, CURRENT_DATE, 1);
+INSERT INTO cust_order VALUES(2, CURRENT_DATE, 2);
+
+INSERT INTO customer VALUES(1, 'Ann', 'Smith', 'ann@gmail.com', 1);
+INSERT INTO customer VALUES(2, 'Boi', 'Tim', 'boi@gmail.com', 2);
