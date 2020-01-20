c130527b37fa0e259756671314f56f9109aca640
przemo06133 <przemo06133@gmail.com>
2020-01-20 21:13:22 +0100

    get user`s orders

--- src/main/java/com/example/thymeleafsql/controllers/rest/CustomerRestController.java
+++ src/main/java/com/example/thymeleafsql/controllers/rest/CustomerRestController.java
@@ -1,13 +1,15 @@
 package com.example.thymeleafsql.controllers.rest;
 
+import com.example.thymeleafsql.models.CustOrder;
 import com.example.thymeleafsql.models.Customer;
 import com.example.thymeleafsql.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customer")
@@ -21,7 +23,19 @@ public class CustomerRestController {
     }
 
     @GetMapping("/all")
-    private List<Customer> getAllCustomers() {
+    public List<Customer> getAllCustomers() {
         return (List<Customer>) customerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Customer> getCustomerById(@PathVariable Integer id) {
+        return customerRepository.findById(id);
+    }
+
+    @GetMapping("/order/{id}")
+    public List<CustOrder> getCustomersByOrderId(@PathVariable Integer id) {
+
+        Optional<Customer> customer = customerRepository.findById(id);
+        return customer.map(Customer::getOrders).orElse(null);
     }
 }
